ec614c353222c561f9c6dbdb90f5df76158fbcd3
ahmedaglan1 <97390857+ahmedaglan1@users.noreply.github.com>
2025-06-29 18:39:51 +0300

    Update PlanData.cs

--- Assets/Scripts/AddPlanesSystem/Core/PlanData.cs
+++ Assets/Scripts/AddPlanesSystem/Core/PlanData.cs
@@ -7,6 +7,7 @@ public class MealEntry
     public string mealType;
     public string mealName;
     public string macros;
+    public string ingredients;
     public string recipe;
     public string category;
 }
